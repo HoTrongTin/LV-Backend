8ccd0ac1562cf83004e089527d9d8d6aae8726be
Hoang Phuc <p070995@gmail.com>
2022-03-29 00:40:24 +0700

    some change

--- app.py
+++ app.py
@@ -354,6 +354,6 @@ atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
     init_spark_streamming()
-    print("List streamming queries: ")
-    print(spark.streams().active)
+    # print("List streamming queries: ")
+    # print(spark.streams().active)
     app.run()

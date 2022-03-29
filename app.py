ac022e77b475102200d999fcb0d7f01f342f8d99
Nguyen Hoang Phuc <p070995@gmail.com>
2022-03-30 00:04:41 +0700

    test foreachBatch

--- app.py
+++ app.py
@@ -274,21 +274,21 @@ def test_cache_query():
 #Schedule jobs
 def cron_cache_query():
     print('Cron job running...')
-    cache_test_streaming_1()
+    #cache_test_streaming_1()
     # merge_silver_d_patients()
 
-def merge_silver_d_patients():
-    spark.sql("""
-MERGE INTO delta.`/medical/silver/d_patients` silver_d_patients
-USING (select * from delta.`/medical/bronze/d_patients`
-where Date_Time > (select CASE WHEN max(Date_Time) is not NULL THEN max(Date_Time) ELSE '2000-01-01 00:00:00' END from delta.`/medical/bronze/d_patients`)
-) updates
-ON silver_d_patients.subject_id = updates.subject_id
-WHEN MATCHED THEN
-  UPDATE SET *
-WHEN NOT MATCHED
-  THEN INSERT *
-""")
+# def merge_silver_d_patients():
+#     spark.sql("""
+# MERGE INTO delta.`/medical/silver/d_patients` silver_d_patients
+# USING (select * from delta.`/medical/bronze/d_patients`
+# where Date_Time > (select CASE WHEN max(Date_Time) is not NULL THEN max(Date_Time) ELSE '2000-01-01 00:00:00' END from delta.`/medical/bronze/d_patients`)
+# ) updates
+# ON silver_d_patients.subject_id = updates.subject_id
+# WHEN MATCHED THEN
+#   UPDATE SET *
+# WHEN NOT MATCHED
+#   THEN INSERT *
+# """)
 
 def cache_test_streaming_1():
     res = spark.read.format("delta").load("/medical/bronze/d_patients")

9293efc4dc65bb6921107328fb0db6dcfb08362b
Nguyen Hoang Phuc <p070995@gmail.com>
2022-03-30 19:28:50 +0700

    test foreachBatch

--- streaming.py
+++ streaming.py
@@ -62,5 +62,5 @@ def init_spark_streaming():
     hadoop_conf.set("fs.s3a.secret.key", "s7C5vkNrc7Dknwe9V+x6m2SFPZyQ2tgUTDz6LDzL")
 
     start_d_patient_stream_bronze()
-    start_d_patient_stream_silver()
+    # start_d_patient_stream_silver()
     #start_admission_stream()

80364ed2baab4306bc3f05f092c7fadf13225f4e
Nguyen Hoang Phuc <p070995@gmail.com>
2022-03-30 09:56:40 +0700

    test foreachBatch

--- app.py
+++ app.py
@@ -281,18 +281,20 @@ def cron_cache_query():
     cache_test_streaming_silver()
     # merge_silver_d_patients()
 
-# def merge_silver_d_patients():
-#     spark.sql("""
-# MERGE INTO delta.`/medical/silver/d_patients` silver_d_patients
-# USING (select * from delta.`/medical/bronze/d_patients`
-# where Date_Time > (select CASE WHEN max(Date_Time) is not NULL THEN max(Date_Time) ELSE '2000-01-01 00:00:00' END from delta.`/medical/bronze/d_patients`)
-# ) updates
-# ON silver_d_patients.subject_id = updates.subject_id
-# WHEN MATCHED THEN
-#   UPDATE SET *
-# WHEN NOT MATCHED
-#   THEN INSERT *
-# """)
+@app.route('/merge-silver-d-patients')
+def merge_silver_d_patients():
+    spark.sql("""
+MERGE INTO delta.`/medical/silver/d_patients` silver_d_patients
+USING (select * from delta.`/medical/bronze/d_patients`
+where Date_Time > (select CASE WHEN max(Date_Time) is not NULL THEN max(Date_Time) ELSE '2000-01-01 00:00:00' END from delta.`/medical/bronze/d_patients`)
+) updates
+ON silver_d_patients.subject_id = updates.subject_id
+WHEN MATCHED THEN
+  UPDATE SET *
+WHEN NOT MATCHED
+  THEN INSERT *
+""")
+    return jsonify({'state': 'successful!'})
 
 def cache_test_streaming_bronze():
     res = spark.read.format("delta").load("/medical/bronze/d_patients")

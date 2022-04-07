5a40f403bc4c4bbc7a8dc74ed09ddae58444b6cb
Hồ Trọng Tín <medigo.tin.ho@gmail.com>
2022-04-07 20:22:01 +0700

    create table

--- manage_schema.py
+++ manage_schema.py
@@ -24,6 +24,7 @@ class ModelDefinition(db.EmbeddedDocument):
 
 class TableDefinition(db.Document):
     project = db.ReferenceField(Project, reverse_delete_rule=CASCADE)
+    name = db.StringField(min_length=1, max_length=45, required=True, unique=True);
     columns = db.ListField(db.EmbeddedDocumentField(ModelDefinition))
 
 
@@ -84,3 +85,92 @@ def update_project(current_user, id):
     project = Project.objects(id = id, user = current_user).update(name = name).save();
 
     return jsonify({'body': project.to_json()})
+
+#TODO: Create table in project
+@app.route('/project/<project_id>/table', methods =['POST'])
+@token_required
+def create_table(current_user, project_id):
+    jsonData = request.get_json()
+    print('------')
+    print(jsonData)
+    print('------')
+    print(project_id)
+  
+    # gets project info
+    name = jsonData['name']
+    # columns = List(jsonData['columns'])
+    columns = []
+    for col in jsonData['columns']:
+        columns.append(col)
+  
+    # checking for existing project
+    project = Project.objects(id = project_id, user = current_user).first()
+
+    if project:
+        new_table = TableDefinition(project = project, name = name);
+
+        # Create columns in table
+        for col in columns:
+            new_table.columns.append(ModelDefinition(name = col['name'], field_type = col['field_type'], nullable = col['nullable']))
+
+        new_table.save()
+
+        return jsonify({'body': new_table.to_json()})
+
+    else:  
+        return make_response('Project does not exist.', 400)
+
+#TODO: Get tables in project
+@app.route('/project/<project_id>/table', methods =['GET'])
+@token_required
+def get_table(current_user, project_id):
+
+    # checking for existing project
+    project = Project.objects(id = project_id, user = current_user).first()
+
+    if project:
+        tables = TableDefinition(project = project);
+
+        return jsonify({'body': tables.to_json()})
+
+    else:  
+        return make_response('Project does not exist.', 400)
+
+#TODO: Create table in project
+@app.route('/project/<project_id>/table/<table_id>', methods =['POST'])
+@token_required
+def update_table(current_user, project_id, table_id):
+    jsonData = request.get_json()
+    print('------')
+    print(jsonData)
+    print('------')
+  
+    # gets project info
+    name = jsonData['name']
+    columns = []
+    for col in jsonData['columns']:
+        columns.append(col)
+  
+    # checking for existing project
+    project = Project.objects(id = project_id, user = current_user).first()
+
+    if project:
+        table = TableDefinition(id = table_id, project = project);
+
+        if table:
+            # Create columns in table
+            
+            table.name = name;
+            table.columns = [];
+
+            for col in columns:
+                table.columns.append(ModelDefinition(name = col['name'], field_type = col['field_type'], nullable = col['nullable']))
+
+            table.save()
+
+            return jsonify({'body': table.to_json()})
+        else:
+            return make_response('Table does not exist.', 400)
+
+    else:  
+        return make_response('Project does not exist.', 400)

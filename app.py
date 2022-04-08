2c398de1dcae4eebfe0687a2f31393dfc1d39399
Nguyen Hoang Phuc <p070995@gmail.com>
2022-04-08 22:12:51 +0700

    fix api

--- app.py
+++ app.py
@@ -81,6 +81,8 @@ def test():
     months = request.json['months']
     print(type(months))
     print(months)
+    print(months[0])
+    months = [int(x) for x in months]
     key = request.args.get('key')
     data = CacheQuery.objects(key=key).first().value
     res = []

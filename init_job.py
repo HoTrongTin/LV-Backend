2917b9959eb3725220cad3d9d147222cace2555a
Nguyen Hoang Phuc <p070995@gmail.com>
2022-04-16 09:21:57 +0700

    updated

--- init_job.py
+++ init_job.py
@@ -43,33 +43,33 @@ def stop_project_streaming(project):
         stopStream(project=project, stream=stream)
 
 #init Schedule jobs
-def cron_data_to_Gold():
-    print('Setup CronJob for copying data from Silver to Gold...')
+# def cron_data_to_Gold():
+#     print('Setup CronJob for copying data from Silver to Gold...')
 
-    # Query all projects
-    projects = Project.objects()
+#     # Query all projects
+#     projects = Project.objects()
 
-    # Query all apis in each project
-    for project in projects:
-        apis = ApisDefinition.objects(project = project)
+#     # Query all apis in each project
+#     for project in projects:
+#         apis = ApisDefinition.objects(project = project)
 
-        # For each stream
-        for api in apis:
-            cache_gold_analysis_query(project_name=project.name, sql=api.sql, key=api.key)
+#         # For each stream
+#         for api in apis:
+#             cache_gold_analysis_query(project_name=project.name, sql=api.sql, key=api.key)
 
-def cron_data_to_mongoDB():
-    print('Schedule jobs copy data from Gold to MongoDB...')
+# def cron_data_to_mongoDB():
+#     print('Schedule jobs copy data from Gold to MongoDB...')
 
-    # Query all projects
-    projects = Project.objects()
+#     # Query all projects
+#     projects = Project.objects()
 
-    # Query all apis in each project
-    for project in projects:
-        apis = ApisDefinition.objects(project = project)
+#     # Query all apis in each project
+#     for project in projects:
+#         apis = ApisDefinition.objects(project = project)
 
-        # For each stream
-        for api in apis:
-            cache_data_to_mongoDB(project_name=project.name, key=api.key)
+#         # For each stream
+#         for api in apis:
+#             cache_data_to_mongoDB(project_name=project.name, key=api.key)
 
 def init_trigger():
     # Setup CronJob for checking streaming
@@ -92,7 +92,8 @@ def init_trigger():
 
         # For each trigger
         for trigger in triggers:
-            start_trigger(project, trigger)
+            if trigger.status == 'ACTIVE':
+                start_trigger(project, trigger)
 
 def start_trigger(project, trigger):
 
